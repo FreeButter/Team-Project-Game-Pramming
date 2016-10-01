0ee549e8035a2202fc7110efb92b751f3e432d61
Scentefy <vampirebane@gmail.com>
2016-10-01 20:32:21 +1300

    Added Model Controller Class
    Added model controller functions
    - Draw
    - Update
    - InitDevices
    - InitResources
    - Reset
    
    Added unique pointer of a model pointer object in the game class

--- Project 1/BasicGameTemplate/Game.cpp
+++ Project 1/BasicGameTemplate/Game.cpp
@@ -12,6 +12,8 @@ using Microsoft::WRL::ComPtr;
 Game::Game()
 {
     m_deviceResources = std::make_unique<DX::DeviceResources>();
+	// TODO: Create Controllers unique pointers
+	m_modelController = std::unique_ptr<ModelController>(new ModelController());
     m_deviceResources->RegisterDeviceNotify(this);
 }
 
@@ -32,7 +34,11 @@ void Game::Initialize(HWND window, int width, int height)
     m_timer.SetFixedTimeStep(true);
     m_timer.SetTargetElapsedSeconds(1.0 / 60);
     */
+
+	
+	
 }
+
 
 #pragma region Frame Update
 // Executes the basic game loop.
@@ -52,9 +58,10 @@ void Game::Update(DX::StepTimer const& timer)
     float elapsedTime = float(timer.GetElapsedSeconds());
 
     // TODO: Add your game logic here.
+	
 	float time = float(timer.GetTotalSeconds());
-
-	m_world = SimpleMath::Matrix::CreateRotationZ(cosf(time) * 2.f);
+	m_modelController->Update(time);
+	//m_world = SimpleMath::Matrix::CreateRotationZ(cosf(time) * 2.f);
 }
 #pragma endregion
 
@@ -74,7 +81,8 @@ void Game::Render()
     auto context = m_deviceResources->GetD3DDeviceContext();
 
     // TODO: Add your rendering code here.
-	m_model->Draw(m_deviceResources->GetD3DDeviceContext(), *m_states, m_world, m_view, m_proj);
+	m_modelController->Draw(m_deviceResources.get());
+	//m_model->Draw(m_deviceResources->GetD3DDeviceContext(), *m_states, m_world, m_view, m_proj);
     
 
     m_deviceResources->PIXEndEvent();
@@ -152,10 +160,12 @@ void Game::GetDefaultSize(int& width, int& height) const
 // These are the resources that depend on the device.
 void Game::CreateDeviceDependentResources()
 {
-    auto device = m_deviceResources->GetD3DDevice();
+	auto device = m_deviceResources->GetD3DDevice();
+    
+	// TODO: Initialize device dependent objects here (independent of window size).
+	m_modelController->InitDevices(m_deviceResources.get());
 
-    // TODO: Initialize device dependent objects here (independent of window size).
-	m_states = std::make_unique<CommonStates> (m_deviceResources->GetD3DDevice());
+	/*m_states = std::make_unique<CommonStates> (m_deviceResources->GetD3DDevice());
 
 	m_fxFactory = std::make_unique<DGSLEffectFactory>(m_deviceResources->GetD3DDevice());
 
@@ -184,7 +194,7 @@ void Game::CreateDeviceDependentResources()
 			fog->SetFogStart(3.f);
 			fog->SetFogEnd(4.f);
 		}
-	});
+	});*/
 }
 
 // Allocate all memory resources that change on a window SizeChanged event.
@@ -192,18 +202,21 @@ void Game::CreateWindowSizeDependentResources()
 {
     // TODO: Initialize windows-size dependent objects here.
 	auto size = m_deviceResources->GetOutputSize();
-	m_view = SimpleMath::Matrix::CreateLookAt(SimpleMath::Vector3(2.f, 2.f, 2.f),
+
+	m_modelController->InitResources((float)size.right, (float)size.bottom);
+	/*m_view = SimpleMath::Matrix::CreateLookAt(SimpleMath::Vector3(2.f, 2.f, 2.f),
 	                                          SimpleMath::Vector3::Zero, SimpleMath::Vector3::UnitY);
 	m_proj = SimpleMath::Matrix::CreatePerspectiveFieldOfView(XM_PI / 4.f,
-		float(size.right) / float(size.bottom), 0.1f, 10.f);
+		float(size.right) / float(size.bottom), 0.1f, 10.f);*/
 }
 
 void Game::OnDeviceLost()
 {
     // TODO: Add Direct3D resource cleanup here.
-	m_states.reset();
-	m_fxFactory.reset();
-	m_model.reset();
+	m_modelController.reset();
+	//m_states.reset();
+	//m_fxFactory.reset();
+	//m_model.reset();
 }
 
 void Game::OnDeviceRestored()
