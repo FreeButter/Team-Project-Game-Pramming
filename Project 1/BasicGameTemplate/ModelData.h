7de5b33a9a67d3159d6b55e1c8818d50d4a75353
Scentefy <vampirebane@gmail.com>
2016-10-06 21:44:16 +1300

    Entity Class Creation By Miguel
    Additions:
    void Init
    void Process
    void SetDead
    bool IsDead
    Get Set Position X Y Z
    Get Set Velocity X Y Z
    Also Created Model Data Class which stores all the models data

---
+++ Project 1/BasicGameTemplate/ModelData.h
@@ -0,0 +1,76 @@
+/**
+DogeyBalls
+ModelController.h
+Purpose: Stores the data needed to manipulate the movement, 
+scale and rotation of the model object
+
+Members are Public as Entity is the only accessor of the object
+
+@author Miguel Saavedra
+@version 1.0 6/10/16
+*/
+#ifndef __MODELDATA_H__
+#define __MODELDATA_H__
+#include <memory>
+#include <Model.h>
+#include <SimpleMath.h>
+
+// Forward Declarations:
+
+class ModelData
+{
+	//Member Methods:
+public:
+	ModelData();
+	~ModelData();
+
+	// Setters 
+
+	// Sets up the model object
+	void SetModel(const char fileLoc);
+
+	// Sets the new Position of the model
+	// Takes x y z float values to manipulate
+	// the next place the model is drawn
+	void SetNewVector(float x, float y, float z);
+
+	// Sets the rotation of the model 
+	// Takes a float value representing degrees
+	void SetNewRotation(float rotation);
+
+	// TODO: Set appropriate value as parameter
+	void SetNewTransform();
+
+protected:
+
+private:
+	//Member Data:
+public:
+	// Unique Pointer to the model object
+	std::unique_ptr<DirectX::Model> m_model;
+
+	// Name of the Model
+	std::string name;
+
+	// Location of Model
+	const char fileLoc;
+
+	// Unique pointer to Vertex Position of Model
+	std::unique_ptr<DirectX::SimpleMath::Vector3> m_pos;
+
+	// Rotation Variable
+	float degrees;
+
+	// Scale/Transform Variable
+	// TODO: Set appropriate variable here to control Scaling
+
+
+	
+
+protected:
+
+private:
+
+};
+
+#endif //__MODELDATA_H__
